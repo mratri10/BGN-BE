757d1a3aaee3b1c9f92e2e0fdb7374777686a6d4
mratri10 <atriariskaalfa10@gmail.com>
2025-07-19 09:06:31 +0700

    cors2

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({origin:'*'}));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(express.json());
 
 app.use('/api/kasatpel', kasatpelRoutes);
